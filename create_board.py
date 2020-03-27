bc8bc734ee56708860ab1e6f4f8e784e535c72b5
roonil-wazlib <elfhogan@gmail.com>
2020-03-27 19:17:55 +1300

    Added code to draw out each of the 9 grids

--- create_board.py
+++ create_board.py
@@ -9,12 +9,13 @@ SMALL_SQUARE_SIZE = SMALL_CELL_SIZE * 3
 SMALL_DIMENSION = SMALL_SQUARE_SIZE * 3
 
 # large grid dimensions
-LARGE_CELL_SIZE = SMALL_CELL_SIZE * 3
+LARGE_CELL_SIZE = SMALL_CELL_SIZE * 2
 LARGE_SQUARE_SIZE = LARGE_CELL_SIZE * 3
 LARGE_DIMENSION = LARGE_SQUARE_SIZE * 3
 
 # window dimensions
-WINDOW_DIMENSION = LARGE_DIMENSION * 2
+PADDING = 25
+WINDOW_DIMENSION = LARGE_DIMENSION * 2 + (PADDING * 4)
 
 
 FPSCLOCK = pygame.time.Clock()
@@ -26,29 +27,58 @@ GRAY = (200, 200, 200)
 
 FPS = 10
 
+
 def draw_small_grid(x, y):
     """ draw a small grid starting at (x, y) coordinates """
     # draw little lines
     for i in range(y, SMALL_DIMENSION + y, SMALL_CELL_SIZE): # vertical
-        pygame.draw.line(DISPLAY, GRAY, (i,y), (i, SMALL_DIMENSION+y))
+        pygame.draw.line(DISPLAY, GRAY, (i,x), (i, SMALL_DIMENSION + x))
     for j in range (x, SMALL_DIMENSION + x, SMALL_CELL_SIZE): # horizontal
-        pygame.draw.line(DISPLAY, GRAY, (x,j), (SMALL_DIMENSION+x, j))
+        pygame.draw.line(DISPLAY, GRAY, (y,j), (SMALL_DIMENSION + y, j))
 
     # draw thick lines
     for i in range(y, SMALL_DIMENSION + y + 1, SMALL_SQUARE_SIZE): # vertical
-        pygame.draw.line(DISPLAY, BLACK, (i,y), (i, SMALL_DIMENSION+y))
+        pygame.draw.line(DISPLAY, BLACK, (i,x), (i, SMALL_DIMENSION + x))
     for j in range (x, SMALL_DIMENSION + x + 1, SMALL_SQUARE_SIZE): # horizontal
-        pygame.draw.line(DISPLAY, BLACK, (x,j), (SMALL_DIMENSION+x, j))
+        pygame.draw.line(DISPLAY, BLACK, (y,j), (SMALL_DIMENSION + y, j))
 
-    return None
+    
+def draw_large_grid(x, y):    
+    """ draw the main grid starting at (x, y) coordinates """
+    # draw little lines
+    for i in range(y, LARGE_DIMENSION + y, LARGE_CELL_SIZE): # vertical
+        pygame.draw.line(DISPLAY, GRAY, (i,x), (i, LARGE_DIMENSION + x))
+    for j in range (x, LARGE_DIMENSION + x, LARGE_CELL_SIZE): # horizontal
+        pygame.draw.line(DISPLAY, GRAY, (y,j), (LARGE_DIMENSION + y, j))
 
+    # draw thick lines
+    for i in range(y, LARGE_DIMENSION + y + 1, LARGE_SQUARE_SIZE): # vertical
+        pygame.draw.line(DISPLAY, BLACK, (i,x), (i, LARGE_DIMENSION + x))
+    for j in range (x, LARGE_DIMENSION + x + 1, LARGE_SQUARE_SIZE): # horizontal
+        pygame.draw.line(DISPLAY, BLACK, (y,j), (LARGE_DIMENSION + y, j))
+        
+        
     
 def main():
     pygame.init()
     pygame.display.set_caption('Pls work') 
     
     DISPLAY.fill(WHITE)
-    draw_small_grid(10,10)
+    # draw the small grids
+    possible_coordinates = [PADDING, int((WINDOW_DIMENSION - SMALL_DIMENSION) / 2), WINDOW_DIMENSION - SMALL_DIMENSION - PADDING]
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                pass
+            else:
+                print(possible_coordinates[i], possible_coordinates[j])
+                draw_small_grid(possible_coordinates[i], possible_coordinates[j])
+                
+    
+    # draw the large grid
+    start_x = start_y = (2 * PADDING) + SMALL_DIMENSION
+    draw_large_grid(start_x, start_y)
+
     
     while True:
         for event in pygame.event.get():
