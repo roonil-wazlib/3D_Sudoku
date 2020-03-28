e075c7108447841443c44f674b0569d189ee0ce8
roonil-wazlib <elfhogan@gmail.com>
2020-03-28 18:06:23 +1300

    Finished highlighting on hover within main Sudoku board

--- create_board.py
+++ create_board.py
@@ -30,6 +30,9 @@ BLACK = (0, 0, 0)
 WHITE = (255,255,255)
 GRAY = (200, 200, 200)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0, 70) #partially transparent
+TURQOISE = (0, 255, 255, 70)
+GREEN = (0, 255, 0, 70)
 
 #font info
 SMALL_FONT_SIZE = 15
@@ -41,6 +44,7 @@ LARGE_FONT = pygame.font.Font('freesansbold.ttf', LARGE_FONT_SIZE)
 POSSIBLE_COORDINATES_1D = [PADDING, int((WINDOW_DIMENSION - SMALL_DIMENSION) / 2), WINDOW_DIMENSION - SMALL_DIMENSION - PADDING]
 POSSIBLE_SMALL_COORDS = []
 LARGE_COORD = (2 * PADDING) + SMALL_DIMENSION
+
 
 def get_all_grid_coordinates():
     for index, x in enumerate(POSSIBLE_COORDINATES_1D):
@@ -97,7 +101,7 @@ def populate_cells_small(board, x, y):
         for j in range(9):
             cell_surf = SMALL_FONT.render('%s' %(board[i][j]), True, BLACK)
             cell_rect = cell_surf.get_rect()
-            cell_rect.topleft = (x + i * SMALL_CELL_SIZE + SMALL_CELL_SIZE - SMALL_FONT_SIZE, y + j * SMALL_CELL_SIZE + SMALL_CELL_SIZE - SMALL_FONT_SIZE)
+            cell_rect.topleft = (x + i * SMALL_CELL_SIZE + SMALL_CELL_SIZE - SMALL_FONT_SIZE + 1, y + j * SMALL_CELL_SIZE + SMALL_CELL_SIZE - SMALL_FONT_SIZE)
             DISPLAY.blit(cell_surf, cell_rect)
     
     
@@ -107,7 +111,7 @@ def populate_cells_large(board, x, y):
         for j in range(9):
             cell_surf = LARGE_FONT.render('%s' %(board[i][j]), True, BLACK)
             cell_rect = cell_surf.get_rect()
-            cell_rect.topleft = (x + i * LARGE_CELL_SIZE + LARGE_CELL_SIZE - LARGE_FONT_SIZE, y + j * LARGE_CELL_SIZE + LARGE_CELL_SIZE - LARGE_FONT_SIZE)
+            cell_rect.topleft = (x + i * LARGE_CELL_SIZE + LARGE_CELL_SIZE - LARGE_FONT_SIZE + 2, y + j * LARGE_CELL_SIZE + LARGE_CELL_SIZE - LARGE_FONT_SIZE - 2)
             DISPLAY.blit(cell_surf, cell_rect)
             
             
@@ -124,7 +128,7 @@ def populate_all_cells(cube):
             else:
                 populate_cells_small(cube[3*i+j], POSSIBLE_COORDINATES_1D[i], POSSIBLE_COORDINATES_1D[j])
         
-    
+
     #populate large cells
     populate_cells_large(cube[4], LARGE_COORD, LARGE_COORD)
     
@@ -136,11 +140,48 @@ def draw_large_box(x, y):
     pygame.draw.rect(DISPLAY, BLUE, (cell_x, cell_y, LARGE_CELL_SIZE, LARGE_CELL_SIZE), 2)
     
     
-def draw_small_box(x, y):
-    """ draw box around small grid"""
     
+def highlight_large_cell(cell_x, cell_y, COLOUR):
+    """highlight the cell the passed in colour"""
+    
+    #pygame.draw.rect(DISPLAY, YELLOW, (cell_x + 1, cell_y + 1, LARGE_CELL_SIZE - 2, LARGE_CELL_SIZE - 2), 0)
+    
+    surf = pygame.Surface((LARGE_CELL_SIZE - 2, LARGE_CELL_SIZE - 2), pygame.SRCALPHA)
+    surf.fill(COLOUR)
+    DISPLAY.blit(surf, (cell_x + 1, cell_y + 1))
+    
+    
+def draw_small_box(x, y):
+    """ draw blue box around small grid"""
+    cell_x = LARGE_COORD + ((x - LARGE_COORD) // LARGE_CELL_SIZE) * LARGE_CELL_SIZE
+    cell_y = LARGE_COORD + ((y - LARGE_COORD) // LARGE_CELL_SIZE) * LARGE_CELL_SIZE    
     pygame.draw.rect(DISPLAY, BLUE, (x, y, SMALL_DIMENSION, SMALL_DIMENSION), 2)
-
+    
+    
+def highlight_relevant_cells(x, y, current_large):
+    """highlight the cells of elements 'related' to hovered over element"""
+    
+    cell_x = LARGE_COORD + ((x - LARGE_COORD) // LARGE_CELL_SIZE) * LARGE_CELL_SIZE
+    cell_y = LARGE_COORD + ((y - LARGE_COORD) // LARGE_CELL_SIZE) * LARGE_CELL_SIZE   
+    
+    cell_x_subsquare = ((cell_x - LARGE_COORD) / LARGE_CELL_SIZE) // 3
+    cell_y_subsquare = ((cell_y - LARGE_COORD) / LARGE_CELL_SIZE) // 3
+    
+    for i in range(LARGE_COORD, LARGE_COORD + LARGE_DIMENSION , LARGE_CELL_SIZE):
+        for j in range(LARGE_COORD, LARGE_COORD + LARGE_DIMENSION , LARGE_CELL_SIZE):
+            
+            if ((i - LARGE_COORD) / LARGE_CELL_SIZE) // 3 == cell_x_subsquare and ((j - LARGE_COORD) / LARGE_CELL_SIZE) // 3 == cell_y_subsquare:
+                if i == cell_x and j == cell_y:
+                    pass
+                elif i == cell_x or j == cell_y:
+                    highlight_large_cell(i, j, TURQOISE)
+                else:
+                    highlight_large_cell(i, j, GREEN)
+                    
+            elif i == cell_x or j == cell_y:
+                highlight_large_cell(i, j, YELLOW)
+          
+                
 
 def in_large_box(x, y):    
     """ check if mouse hovering over large Sudoku """
@@ -159,7 +200,8 @@ def in_small_box(x, y):
         
     return False, None
     
-def update_display(cube, mouse_x=None, mouse_y=None):
+    
+def update_display(cube, current_large, mouse_x=None, mouse_y=None):
     """ redraw display """
     DISPLAY.fill(WHITE)
     draw_all_grids()
@@ -167,6 +209,7 @@ def update_display(cube, mouse_x=None, mouse_y=None):
     
     if mouse_x is not None and mouse_y is not None:
         if in_large_box(mouse_x, mouse_y):
+            highlight_relevant_cells(mouse_x, mouse_y, current_large)
             draw_large_box(mouse_x, mouse_y)
         in_small, coords = in_small_box(mouse_x, mouse_y)
         if in_small:
@@ -182,7 +225,9 @@ def main():
     mouse_y = 0
     
     cube = Sudoku3D(generate_3d_board())
-    update_display(cube)
+    current_large = 4
+    update_display(cube, current_large)
+    
     
     while True: #main game loop
         has_clicked = False
@@ -200,7 +245,7 @@ def main():
     
         
         # redraw everything
-        update_display(cube, mouse_x, mouse_y)    
+        update_display(cube, current_large, mouse_x, mouse_y)    
     
         pygame.display.update()    
         FPSCLOCK.tick(FPS)        
